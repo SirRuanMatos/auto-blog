3a95ce8607afe1360709f9c68acfaa677b244e8f
Ruan Matos <ruan.matos@nsh.com.br>
2025-12-10 16:41:09 -0300

    Changing header title color

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -12,7 +12,7 @@ export default function Header() {
             <div className="flex justify-between w-full py-8 items-center">
                 <h1
                     onClick={handleTitleClick}
-                    className="text-texts-main text-2xl md:text-3xl font-medium cursor-pointer"
+                    className="text-hovers text-2xl md:text-3xl font-medium cursor-pointer"
                 >
                     Ruans AutoBlog
                 </h1>
